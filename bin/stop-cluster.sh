36e40fb3fee0952442428e409624edca72642edd
Hector Castro <hectcastro@gmail.com>
2014-04-05 10:12:12 -0400

    Remove containers with -f instead of kill and rm.

--- bin/stop-cluster.sh
+++ bin/stop-cluster.sh
@@ -8,8 +8,7 @@ fi
 
 for container in $(docker ps | grep "hectcastro/riak" | cut -d" " -f1);
 do
-  docker kill "${container}" > /dev/null 2>&1
-  docker rm "${container}" > /dev/null 2>&1
+  docker rm -f "${container}" > /dev/null 2>&1
 done
 
 echo "Stopped the cluster and cleared all of the running containers."
