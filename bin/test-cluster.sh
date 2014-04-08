05fca34a34c2f31850f3b54eb6001e0012d280ae
Hector Castro <hectcastro@gmail.com>
2014-04-08 14:39:31 -0400

    Add automatic clustering capability and switch to phusion/baseimage.

--- bin/test-cluster.sh
+++ bin/test-cluster.sh
@@ -1,11 +1,12 @@
 #! /bin/bash
 
 set -e
-# set -x
 
-SEED_CONTAINER_ID=$(docker ps | egrep "riak01" | cut -d" " -f1)
-SEED_SSH_PORT=$(docker port "$SEED_CONTAINER_ID" 22 | cut -d ":" -f2)
+if env | grep -q "DOCKER_RIAK_DEBUG"; then
+  set -x
+fi
 
-sshpass -p "basho" \
-  ssh -o "StrictHostKeyChecking no" -o "UserKnownHostsFile /dev/null" -o "LogLevel quiet" -p "$SEED_SSH_PORT" root@localhost \
-    riak-admin member-status
+RANDOM_CONTAINER_ID=$(docker ps | egrep "hectcastro/riak" | cut -d" " -f1 | perl -MList::Util=shuffle -e'print shuffle<>' | head -n1)
+CONTAINER_HTTP_PORT=$(docker port "${RANDOM_CONTAINER_ID}" 8098 | cut -d ":" -f2)
+
+curl -s "http://localhost:${CONTAINER_HTTP_PORT}/stats" | python -mjson.tool
