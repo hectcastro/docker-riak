c371bb30a15044b4eb80e982011a16bff25c57ca
Hector Castro <hectcastro@gmail.com>
2014-05-27 11:16:04 -0400

    Set an explicit inet_dist port range.

--- bin/riak.sh
+++ bin/riak.sh
@@ -12,6 +12,11 @@ ulimit -n 4096
 # Ensure the Erlang node name is set correctly
 sed -i.bak "s/127.0.0.1/${IP_ADDRESS}/" /etc/riak/vm.args
 
+if env | grep -q "DOCKER_RIAK_INET_DIST_MIN" && ! egrep -q "inet_dist_listen_min" /etc/riak/vm.args; then
+  echo "-kernel inet_dist_listen_min ${DOCKER_RIAK_INET_DIST_MIN}" >> /etc/riak/vm.args
+  echo "-kernel inet_dist_listen_max ${DOCKER_RIAK_INET_DIST_MAX}" >> /etc/riak/vm.args
+fi
+
 # Start Riak
 exec /sbin/setuser riak "$(ls -d /usr/lib/riak/erts*)/bin/run_erl" "/tmp/riak" \
    "/var/log/riak" "exec /usr/sbin/riak console"
