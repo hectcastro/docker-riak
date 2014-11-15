4127dad018da835e5d8ff585c6f35a8939a260d0
Hector Castro <hectcastro@gmail.com>
2014-11-15 13:15:45 -0500

    Add support for Docker over a UNIX socket
    This changeset adjusts the health checks associated with Riak so
    that they work when `DOCKER_HOST` is set to a UNIX socket.
    
    Attempts to resolve #32 and #33.

--- bin/test-cluster.sh
+++ bin/test-cluster.sh
@@ -6,7 +6,12 @@ if env | grep -q "DOCKER_RIAK_DEBUG"; then
   set -x
 fi
 
-CLEAN_DOCKER_HOST=$(echo "${DOCKER_HOST}" | cut -d'/' -f3 | cut -d':' -f1)
+if [[ "${DOCKER_HOST}" == unix://* ]]; then
+  CLEAN_DOCKER_HOST="localhost"
+else
+  CLEAN_DOCKER_HOST=$(echo "${DOCKER_HOST}" | cut -d'/' -f3 | cut -d':' -f1)
+fi
+
 RANDOM_CONTAINER_ID=$(docker ps | egrep "hectcastro/riak" | cut -d" " -f1 | perl -MList::Util=shuffle -e'print shuffle<>' | head -n1)
 CONTAINER_HTTP_PORT=$(docker port "${RANDOM_CONTAINER_ID}" 8098 | cut -d ":" -f2)
 
